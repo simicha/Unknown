ff75d744e85bec40c737a0b85999b33792849a83
Andres2D <andres2d1997.1206@gmail.com>
2022-07-13 16:59:01 -0500

    Add form references to inputs

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet, Text, TextInput, Dimensions } from 'react-native';
 import ActionsButtons from './components/ActionButtons/index';
 
-const {width, height} = Dimensions.get('screen')
+const {width, height} = Dimensions.get('screen');
 
+const isValid = (input) => {
+  if(!input) {
+    return false;
+  }
+  return input.trim() !== '' ? true : false;
+}
 
 export default function App() {
+  const countryCodeRef = useRef();
+  const phoneNumberRef = useRef();
+  const messageRef = useRef();
+
+  const submitHandler = () => {
+
+    if(isValid(countryCodeRef.current.value) && isValid(phoneNumberRef.current.value) && isValid(messageRef.current.value)) {
+      const form = {
+        countryCode: countryCodeRef.current.value,
+        phoneNumber: phoneNumberRef.current.value,
+        message: messageRef.current.value
+      };
+      // do request
+      console.log(form);
+      resetForm();
+    }else{
+      // invalid form
+      console.log('Invalid form');
+    }
+  };
+
+  const resetForm = () => {
+    countryCodeRef.current.value= '';
+    phoneNumberRef.current.value= '';
+    messageRef.current.value= '';  
+  };
+
   return (
     <View style={styles.containerMain}>
       <View style={styles.containerSVG}></View>
       <View style={styles.container}>
         <Text style={styles.title}>Write your message</Text>
         <Text style={styles.subtitle}>Write the message you will send to the unknown number!</Text>
-        <TextInput style={styles.textInput}
-          placeholder="Country"/>
-        <TextInput style={styles.textInput}
-          placeholder="Phone Number"/>
-        <TextInput style={styles.textInput}
-          placeholder="Message"/>
-        <ActionsButtons/>
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Country code"
+          ref={countryCodeRef}
+        />
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Phone Number"
+          ref={phoneNumberRef}
+        />
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Message"
+          ref={messageRef}
+        />
+        <ActionsButtons 
+          action={submitHandler}
+        />
         <StatusBar style='auto'/>
       </View>
     </View>
